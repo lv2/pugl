3359c78b197a1812023e52111699f74d110be37e
David Robillard <d@drobilla.net>
2019-07-25 14:15:55 +0200

    Mac: Fix build on MacOS older than 10.10

--- pugl/pugl_osx.m
+++ pugl/pugl_osx.m
@@ -34,6 +34,12 @@
 
 #include <stdlib.h>
 
+#ifndef __MAC_10_10
+#define NSOpenGLProfileVersion4_1Core NSOpenGLProfileVersion3_2Core
+typedef NSUInteger NSEventModifierFlags;
+typedef NSUInteger NSWindowStyleMask;
+#endif
+
 @class PuglOpenGLView;
 
 struct PuglInternalsImpl {
