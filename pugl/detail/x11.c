8fda9c8ba5768e32b753167174102abca8095f0e
Zoë Sparks <zoe@milky.flowers>
2020-02-09 18:35:53 -0500

    X11: Fix XMoveResizeWindow error check
    In the Xlib standard, a return value of 0 is defined as indicating an
    error (see /Xlib - C Language X Interface/, Ch. 1, "Errors").
    XMoveResizeWindow currently returns 1 on success in conformance with
    this. However, the error check in puglSetFrame assumes that 0 indicates
    a sucessful resize, causing PUGL_UNKNOWN_ERROR to be returned whenever
    the resize succeeds. Flip the check to match the Xlib convention.

--- pugl/detail/x11.c
+++ pugl/detail/x11.c
@@ -872,7 +872,7 @@ puglSetFrame(PuglView* view, const PuglRect frame)
 	view->frame = frame;
 
 	if (view->impl->win &&
-	    XMoveResizeWindow(view->world->impl->display, view->impl->win,
+	    !XMoveResizeWindow(view->world->impl->display, view->impl->win,
 	                      (int)frame.x, (int)frame.y,
 	                      (int)frame.width, (int)frame.height)) {
 		return PUGL_UNKNOWN_ERROR;
