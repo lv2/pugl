db8a7cce31e35cb821127b9b3eb2c109cdbe52eb
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-05-24 00:13:44 +0200

    Fix the declaration of WGL function types

--- src/win_gl.c
+++ src/win_gl.c
@@ -52,9 +52,9 @@
 #define WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB 0x00000002
 #define WGL_CONTEXT_DEBUG_BIT_ARB 0x00000001
 
-typedef HGLRC (*WglCreateContextAttribs)(HDC, HGLRC, const int*);
-typedef BOOL (*WglSwapInterval)(int);
-typedef BOOL (
+typedef HGLRC (WINAPI *WglCreateContextAttribs)(HDC, HGLRC, const int*);
+typedef BOOL (WINAPI *WglSwapInterval)(int);
+typedef BOOL (WINAPI
   *WglChoosePixelFormat)(HDC, const int*, const FLOAT*, UINT, int*, UINT*);
 
 typedef struct {
